a4c8aacf11d32f329b8a4ebc08f345925329a821
user.emial <emil.mehraliyev003@gmail.com>
2024-07-10 23:18:16 +0300

    HW35

--- src/telran/io/CodeCommentsSeparation.java
+++ src/telran/io/CodeCommentsSeparation.java
@@ -1,15 +1,46 @@
 package telran.io;
 
+import java.io.*;
+
 public class CodeCommentsSeparation {
 
 	public static void main(String[] args) {
-		//args[0] - file path for file containing both Java class code and comments
-		//args[1] - result file with only code
-		//args[2] -result file with only comments
-		// example of args[0] "src/telran/io/test/InputOutputTest.java" 
-		//TODO 
-		//from one file containing code and comments to create two files
-		//one with only comments and second with only code
-	}
+		if (args.length != 3) {
+			System.err.println("Usage: java CodeCommentsSeparation <inputFilePath> <codeFilePath> <commentsFilePath>");
+			System.exit(1);
+		}
 
+		String inputFilePath = args[0];
+		String codeFilePath = args[1];
+		String commentsFilePath = args[2];
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+				BufferedWriter codeWriter = new BufferedWriter(new FileWriter(codeFilePath));
+				BufferedWriter commentsWriter = new BufferedWriter(new FileWriter(commentsFilePath))) {
+
+			String line;
+			boolean isCommentBlock = false;
+
+			while ((line = reader.readLine()) != null) {
+				line = line.trim();
+
+				if (line.startsWith("//") || isCommentBlock || line.startsWith("/*")) {
+					commentsWriter.write(line);
+					commentsWriter.newLine();
+					if (line.startsWith("/*")) {
+						isCommentBlock = true;
+					}
+					if (line.endsWith("*/")) {
+						isCommentBlock = false;
+					}
+				} else {
+					codeWriter.write(line);
+					codeWriter.newLine();
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
